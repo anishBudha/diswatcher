f59cf048a68c7e80d46aa8190c00de88ca4f262f
anishBudha <anishbudha5628@gmail.com>
2024-01-11 00:49:57 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,30 +1,55 @@
-# discord-link-opener
-Automatically open browser tabs when links matching given constraints are sent in discord channels.
+# Discord Link Opener
 
-discord.py updates have depreceated the previous version of this script. 
-In order to run it now, you must install discord.py-self
+Automatically open browser tabs when links matching given constraints are sent in Discord channels.
 
-# Installation and Usage
-1.    Download Python 3.6.x or 3.7.x . Before installing, make sure to check “Add Python to PATH”.
-2.   Once installed, open CMD and type: 
-                     pip install discord.py-self and 
-                     pip install asyncio
-3.    Download Link Opener: https://github.com/clearyy/discord-link-opener
-4.    Copy open.py to your desktop.
-5.    Right click open.py and select “Edit with IDLE”. Once in the code, only do the following two things:
-                     Add the discord channel IDs (separated by commas) that you would like to monitor.
-                     Add your Discord token. (Tutorial on how to find your token: 
-                        https://www.youtube.com/watch?v=tI1lzqzLQCs)
-      Do not edit the keyword and blacklist lines.
-6.    Save the file.
-7.    Run open.py and you will be prompted for keywords and blacklisted words. Enter words in lowercase, separated by spaces and press enter when completed.
-8.    Wait for the bot to automatically open Chrome browser tabs when links matching given constraints are sent in the specified discord channels. 
-9.    Cook.
-10.   To change keywords at any point, press Ctrl + c to terminate the script. Then simply run the script again and enter new words when prompted.
+**Note:** discord.py updates have deprecated the previous version of this script. To run it now, you must install discord.py-self.
 
-# Requirements
-asyncio, discord.py-self
+## Installation and Usage
 
-# Operating Systems
-This was designed for and only tested on windows.
+> Link must be in URL format (protocol, domain name, and path).
 
+1. Download [Python 3.6.x or higher](https://www.python.org/downloads/). Before installing, make sure to check “Add Python to PATH”.
+
+2. Once installed, open CMD and type:
+
+   ```bash
+   pip install discord.py-self
+   pip install asyncio
+   git clone https://github.com/dolfies/discord.py-self
+   cd discord.py-self
+   python3 -m pip install -U .
+   ```
+
+---
+
+3. Download and run [diswatcher.exe](google.com).
+   > OR
+4. Download Files from github.
+
+5. Copy `open.py` to your desktop.
+
+6. Run `open.py` and follow the prompts:
+
+   - **Step 1:** Enter your Discord token. ([Watch this video to know how to get your Discord token](https://www.youtube.com/watch?v=YEgFvgg7ZPI&ab_channel=GaugingGadgets))
+   - **Step 2:** Enter Discord channel IDs (separated by commas) that you would like to monitor. (To get channeld id, right click on the channel name and select copy id)
+
+   - **Step 3:** Enter whitelisted words (keywords) in lowercase, separated by spaces.
+   - **Step 4:** Enter blacklisted words in lowercase, separated by spaces.
+
+   **Note:** Do not edit the keyword and blacklist lines in the code.
+
+7. Save the file.
+
+8. Run `open.py`, and the bot will automatically open Chrome browser tabs when links matching given constraints are sent in the specified Discord channels.
+
+9. To change keywords at any point, press `Ctrl + c` to terminate the script. Then simply run the script again and enter new words when prompted.
+
+## Requirements
+
+- asyncio
+- discord.py-self
+- keyborad
+
+## Operating Systems
+
+This was designed for and only tested on Windows.
--- open.py
+++ open.py
@@ -4,15 +4,36 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import re
+import sys
+import shutil
+import keyboard
+import time
 
-'''
-by cleary#6546 // @preorderd
-'''
+def print_horizontal_line():
+    terminal_width, _ = shutil.get_terminal_size()
+    print('-' * terminal_width)
 
-#pylint: disable=anomalous-backslash-in-string
+# Redirect stdout and stderr to a file
+sys.stderr = open('bot_errors.log', 'w')
+
+
+print(f'+----------------------------------------------------------------------+')
+print(f'        DO NOT FORGET TO CHANGE THE PREFERRED BROWSER TO DEFAULT       ')
+print(f'+----------------------------------------------------------------------+')
+print('CC: cleary#6546 / @preorderd')
+print('Moded by Anish Budha')
+print_horizontal_line()
+
+
 
 client = Bot('adawd@@#^^')
 client.remove_command('help')
+
+token = input("Please enter the discord token: ")
+
+#enter channel id(s) where links would be picked up (monitor channel id) seperated by commas. these should be ints
+channel_ids_input = input("Enter channel IDs separated by space: ")
+channels = [int(channel_id) for channel_id in channel_ids_input.split()]
 
 #prompt user enter keywords to check for in links
 keywords = list(map(str,input("Enter keywords seperated by space: ").split()))
@@ -20,22 +41,29 @@ keywords = list(map(str,input("Enter keywords seperated by space: ").split()))
 #prompt user to enter negative keywords that will prevent a browser window from opening to have no blacklisted words, press enter right away
 blacklist = list(map(str,input("Enter blacklisted keywords seperated by space: ").split()))
 
-#enter channel id(s) where links would be picked up (monitor channel id) seperated by commas. these should be ints
-channels = []
+
 
 #enter token of discord account that has access to watch specified channels
-token = ''
 
 global start_count
 start_count = 0
+
 
 #check for keywords and blacklisted words in message urls and open browser if conditions are met
 async def check_urls(urls):
     for url in urls:
         if any(x in url.lower() for x in keywords) and all(x not in url.lower() for x in blacklist):
             #enter path to chrome here, for windows 10, this should work
-            webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open(url)
-            print(f'Opened {url}')
+            webbrowser.open_new(url)
+            print_horizontal_line()
+            print(f'Opened: {url}')
+            print_horizontal_line()
+
+print("Starting", end="", flush=True)
+for _ in range(5):
+    time.sleep(1)
+    print(".", end="", flush=True)
+print('\n')
 
 @client.event
 async def on_message(message):
@@ -43,15 +71,15 @@ async def on_message(message):
     # temporary bypass to weird d.py cacheing issue
     # only print this info on the first time the client launches. this is due to d.py calling on_ready() after the bot regains connection
     if start_count == 0:
-        print('\n{} is ready to cop some restocks.\n'.format(str(client.user)))
         if len(keywords) >= 1 and keywords[0] != '':
-            print('Watching for keywords {}.\n'.format(', '.join(keywords)))
+            print('+ Watching for keywords:  {}.\n'.format(', '.join(keywords)))
         else:
-            print('No keywords have been provided.\n')
+            print('+ No keywords have been provided.\n')
         if len(blacklist) > 0:
-            print('Ignoring keywords {}.\n'.format(', '.join(blacklist)))
+            print('+ Ignoring keywords    :  {}.\n'.format(', '.join(blacklist)))
         else:
-            print('No keywords currently blacklisted.\n')
+            print('+ No keywords currently blacklisted.')
+        print('+ {} is listening...\n'.format(str(client.user)))
         start_count += 1
     else:
         if message.channel.id in channels:
@@ -76,9 +104,13 @@ async def on_message(message):
                         except:
                             pass
             if message.content != '':
-                print(message.content)
+                # print_horizontal_line()
+                print(f'\nURLs: \n{message.content}')
+                # print_horizontal_line()
                 urls4 = re.findall("(?:(?:https?|ftp):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()\[\]{};:'.,<>?«»“”‘’]))?",message.content)
                 if urls4:
                     await check_urls(urls4)
+            
+keyboard.add_hotkey('q', lambda: client.loop.create_task(client.close()))
 
 client.run(token)
